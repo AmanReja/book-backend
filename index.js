f744da90654272324d60acc74abbd1db69f680b4
Aman Reja <amnmolla742@gmail.com>
2025-06-17 21:46:27 +0530

    fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://book-backend-ust3.onrender.com",
   })
 );
 // app.use(express.static(path.join(__dirname, "build")));
